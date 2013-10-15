9d3e0f500825153bc60355908ef1da2369fbed01
Parsha Pourkhomami <parshap+git@gmail.com>
2013-10-14 23:12:02 -0700

    Handle pseudo elements and classes

--- index.js
+++ index.js
@@ -14,22 +14,25 @@ module.exports = function(css, html) {
 	var ast = parse(css, { position: true });
 	var doc = jsdom(html, null, JSDOM_OPTIONS);
 	var rules = getRules(ast.stylesheet);
-	var unused = rules.filter(isUnused);
+	var dead = rules.filter(isDead);
 	var lines = css.split("\n");
 
+	// Copy css string into retval string except for parts owned by dead rules
 	var cur = 0;
 	var retval = "";
-	unused.forEach(function(rule) {
+	dead.forEach(function(rule) {
 		var start = positionToIndex(rule.position.start),
 			end = positionToIndex(rule.position.end);
 		retval += css.slice(cur, start);
 		cur = end;
 	});
 
+	// Copy any remaining css after last dead rule
 	retval += css.slice(cur);
 
 	return retval;
 
+	// Convert a line number and column position to index in css string
 	function positionToIndex(position) {
 		return lines.slice(position.line).reduce(function(acc, line) {
 			return acc + line.length;
@@ -37,14 +40,33 @@ module.exports = function(css, html) {
 	}
 
 	// Return true if rule's selectors were not used
-	function isUnused(rule) {
+	function isDead(rule) {
 		return rule.selectors.every(function(selector) {
+			selector = stripPseudos(selector);
 			// Return true if this selector was not used
 			return ! doc.querySelector(selector);
 		});
 	}
 };
 
+// Remove any pseudo classes or elements from the selector
+// @TODO We may want to keep some pseudo classes (e.g., :nth-child())
+var stripPseudos = (function() {
+	var parseCSS = require("slick").parse,
+		forEach = Array.prototype.forEach;
+
+	return function(selector) {
+		var expressions = parseCSS(selector);
+		forEach.call(expressions, function(expression) {
+			forEach.call(expression, function(part) {
+				part.pseudos = null;
+			});
+		});
+		return expressions.toString();
+	};
+})();
+
+// Flatten all "rule" objects in AST into array
 function getRules(ast) {
 	var rules = [];
 	walkAST(ast, function(ast) {
@@ -55,7 +77,7 @@ function getRules(ast) {
 	return rules;
 }
 
-// Recursively walk all AST rule objects
+// Recursively walk all AST nodes
 function walkAST(ast, fn) {
 	fn(ast);
 
